5ccf9cdae89094d151fdf5423e2aaddde63916af
jeffrey <piccirilloj1@gmail.com>
2022-12-30 16:47:05 -0600

    Vue.config.js needs to serve assets from nose-hht subdirectory, bad merge changed that to nose-hht-dev

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ const webpackParams = {
 };
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/nose-hht-dev/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/nose-hht/' : '/',
   transpileDependencies: ['vuetify'],
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
