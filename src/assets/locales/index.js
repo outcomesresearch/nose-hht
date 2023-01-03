7283af096c895b76a662be9ca86dda282cc06a8f
jeffrey <piccirilloj1@gmail.com>
2023-01-03 14:25:19 -0600

    More general language support - add language-specific fallbacks
    - AKA, if browser passes simply 'fr' but not fr-CA or fr-FR, app knows to serve the closest thing it has (fr_CA) instead of english which is the global fallback

--- src/assets/locales/index.js
+++ src/assets/locales/index.js
@@ -6,10 +6,15 @@ import it_it from './it_it';
 import fr_ca from './fr_ca';
 
 export default {
+  en: en_us,
   'en-US': en_us,
+  es: es_us,
   'es-US': es_us,
   'es-ES': es_sp,
+  nl: nl_nl,
   'nl-NL': nl_nl,
+  it: it_it,
   'it-IT': it_it,
+  fr: fr_ca,
   'fr-CA': fr_ca,
 };
